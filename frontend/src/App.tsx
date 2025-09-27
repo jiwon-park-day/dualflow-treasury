9c9acfb4fd52b8fa0e50115932619626702ed1d6
jiwon-park-day <jiwon.park1231@gmail.com>
2025-09-26 22:37:41 -0600

    Create credit card page with placeholder layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HeroPage from './components/hero/HeroPage';
 import Layout from './components/common/Layout';
 import Dashboard from './components/dashboard/Dashboard';
-// import CreditCardPage from './components/credit-card/CreditCardPage';
+import CreditCardPage from './components/credit-card/CreditCardPage';
 // import BankingPage from './components/banking/BankingPage';
 // import CheckingPage from './components/banking/CheckingPage';
 // import InvestmentPage from './components/banking/InvestmentPage';
@@ -30,8 +30,8 @@ function App() {
         
         {/* All app pages wrapped in Layout */}
         <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
-        {/* <Route path="/credit-card" element={<Layout><CreditCardPage /></Layout>} />
-        <Route path="/banking" element={<Layout><BankingPage /></Layout>} />
+        <Route path="/credit-card" element={<Layout><CreditCardPage /></Layout>} />
+        {/* <Route path="/banking" element={<Layout><BankingPage /></Layout>} />
         <Route path="/banking/checking" element={<Layout><CheckingPage /></Layout>} />
         <Route path="/banking/investment" element={<Layout><InvestmentPage /></Layout>} />
         <Route path="/settings" element={<Layout><SettingsPage /></Layout>} /> */}
